661e4a0919fa3607c941e716f7120ca2eeb56567
ilya034 <68101303+ilya034@users.noreply.github.com>
2025-02-12 01:48:57 +0500

    Create basic_config.sh

---
+++ basic_config.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+while getopts u: flag
+do
+    case "${flag}" in
+        u) newusername=${OPTARG};;
+    esac
+done
+
+apt update && apt upgrade -y
+
+echo install xray
+bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install --beta
+
+echo install nginx
+sudo apt install curl gnupg2 ca-certificates lsb-release ubuntu-keyring
+curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor \
+    | sudo tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null
+gpg --dry-run --quiet --no-keyring --import --import-options import-show /usr/share/keyrings/nginx-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] \
+http://nginx.org/packages/ubuntu `lsb_release -cs` nginx" \
+    | sudo tee /etc/apt/sources.list.d/nginx.list
+echo -e "Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900\n" \
+    | sudo tee /etc/apt/preferences.d/99nginx
+apt update
+apt install nginx
+
+echo install acme.sh
+wget -O -  https://get.acme.sh | sh -s email=$newusernamek@gmail.com
+
+echo add new user
+useradd $newusername -m -G sudo -s /bin/bash
+passwd $newusername
+
+echo configure key auth
+mkdir /home/$newusername/.ssh
+touch /home/$newusername/.ssh/authorized_keys
+chown $newusername:$newusername -R /home/$newusername/.ssh
+chmod 700 /home/$newusername/.ssh
+chmod 600 /home/$newusername/.ssh/authorized_keys
