0fc2785008d0bc0bc697988bc014c1796f3a91c3
ilya034 <68101303+ilya034@users.noreply.github.com>
2025-02-13 00:27:18 +0500

    Update basic_config.sh

--- basic_config.sh
+++ basic_config.sh
@@ -3,18 +3,22 @@
 while getopts u: flag
 do
     case "${flag}" in
-        u) newusername=${OPTARG};;
+        u) new_user_name=${OPTARG};;
+        p) new_ssh_port=${OPTARG};;
     esac
 done
 
 apt update && apt upgrade -y
+clear
 
 echo add new user
-useradd $newusername -m -G sudo -s /bin/bash
-passwd $newusername
+useradd $new_user_name -m -G sudo -s /bin/bash
+passwd $new_user_name
+clear
 
 echo install xray
-bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install --beta -u &newusername
+bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install --beta -u $new_user_name
+clear
 
 echo configure xray logs
 cd /var/log
@@ -22,6 +26,7 @@ touch xray/access.log xray/error.log
 chmod 775 -R xray
 chown nobody:nogroup -R xray
 cd
+clear
 
 echo install nginx
 sudo apt install curl gnupg2 ca-certificates lsb-release ubuntu-keyring
@@ -34,17 +39,19 @@ http://nginx.org/packages/ubuntu `lsb_release -cs` nginx" \
 echo -e "Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900\n" \
     | sudo tee /etc/apt/preferences.d/99nginx
 apt update
-apt install nginx
+apt install nginx -y
+clear
 
 echo install acme.sh
-wget -O -  https://get.acme.sh | sh -s email="$newusername"k@gmail.com
+wget -O -  https://get.acme.sh | sh -s email="$new_user_name"k@gmail.com
+clear
 
 echo install vimrc
 curl -sLO https://github.com/ilya034/VPSBasicConfigure/raw/master/.vimrc
 
 echo configure key auth
-mkdir /home/$newusername/.ssh
-touch /home/$newusername/.ssh/authorized_keys
-chown $newusername:$newusername -R /home/$newusername/.ssh
-chmod 700 /home/$newusername/.ssh
-chmod 600 /home/$newusername/.ssh/authorized_keys
+mkdir /home/$new_user_name/.ssh
+touch /home/$new_user_name/.ssh/authorized_keys
+chown $new_user_name:$new_user_name -R /home/$new_user_name/.ssh
+chmod 700 /home/$new_user_name/.ssh
+chmod 600 /home/$new_user_name/.ssh/authorized_keys
