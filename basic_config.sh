4926d2c4789eef29968a42899b201bffc9f4f5a2
ilya034 <68101303+ilya034@users.noreply.github.com>
2025-07-04 21:22:12 +0500

    Update basic_config.sh

--- basic_config.sh
+++ basic_config.sh
@@ -1,63 +1,53 @@
 #!/bin/bash
 
-while getopts :u:p:e flag
+set -e
+
+NEW_USER_NAME=ilya034
+PUBLIC_SSH_KEY=""
+NEW_SSH_PROT=22
+
+while getopts :u:k:p flag
 do
     case "${flag}" in
-        u) new_user_name=${OPTARG};;
-        p) new_ssh_port=${OPTARG};;
-        e) acme_email=${OPTARG};;
+        u) NEW_USER_NAME=${OPTARG};;
+        k) PUBLIC_SSH_KEY=${OPTARG};;
+        p) NEW_SSH_PORT=${OPTARG};; 
     esac
 done
 
 apt update && apt upgrade -y
-clear
+apt install htop vim wget curl speedtest-cli gnupg2 ca-certificates lsb-release ubuntu-keyring -y
 
-echo add new user
-useradd $new_user_name -m -G sudo -s /bin/bash
-passwd $new_user_name
-clear
-
-echo install xray
-useradd -M xray
-bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install --beta -u xray
-clear
-
-echo install nginx
-sudo apt install curl gnupg2 ca-certificates lsb-release ubuntu-keyring
 curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor \
-    | sudo tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null
+    | tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null
+
 gpg --dry-run --quiet --no-keyring --import --import-options import-show /usr/share/keyrings/nginx-archive-keyring.gpg
+
 echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] \
 http://nginx.org/packages/ubuntu `lsb_release -cs` nginx" \
-    | sudo tee /etc/apt/sources.list.d/nginx.list
+    | tee /etc/apt/sources.list.d/nginx.list
+
 echo -e "Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900\n" \
-    | sudo tee /etc/apt/preferences.d/99nginx
+    | tee /etc/apt/preferences.d/99nginx
+
 apt update
 apt install nginx -y
-clear
 
-echo install acme.sh
-wget -O -  https://get.acme.sh | sh -s email="$acme_email"
-clear
+useradd -M xray
+bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install --beta -u xray
 
-echo install certbot
-sudo apt update && sudo apt install python3 python3-venv libaugeas0
-sudo python3 -m venv /opt/certbot/
-sudo /opt/certbot/bin/pip install --upgrade pip
-sudo /opt/certbot/bin/pip install certbot certbot-nginx && sudo ln -s /opt/certbot/bin/certbot /usr/bin/certbot
+useradd $NEW_USER_NAME -m -G sudo -s /bin/bash
+passwd $NEW_USER_NAME
 
-echo install vimrc
 curl -sLO https://github.com/ilya034/VPSBasicConfigure/raw/master/.vimrc
+cp .vimrc /home/$NEW_USER_NAME/
 
-echo configure key auth
-mkdir /home/$new_user_name/.ssh
-touch /home/$new_user_name/.ssh/authorized_keys
-chown $new_user_name:$new_user_name -R /home/$new_user_name/.ssh
-chmod 700 /home/$new_user_name/.ssh
-chmod 600 /home/$new_user_name/.ssh/authorized_keys
-clear
+mkdir -p /home/${NEW_USER_NAME}/.ssh
+echo "${PUBLIC_SSH_KEY}" > /home/${NEW_USER_NAME}/.ssh/authorized_keys
+chown -R ${NEW_USER_NAME}:${NEW_USER_NAME} /home/${NEW_USER_NAME}/.ssh 
+chmod 700 /home/${NEW_USER_NAME}/.ssh
+chmod 600 /home/${NEW_USER_NAME}/.ssh/authorized_keys
 
-echo edit sshd_config
 curl -sLo /etc/ssh/sshd_config.d/updated.conf https://github.com/ilya034/VPSBasicConfigure/raw/master/sshd.conf
-echo "Port $new_ssh_port" >> /etc/ssh/sshd_config.d/updated.conf
+echo "Port $NEW_SSH_PORT" >> /etc/ssh/sshd_config.d/updated.conf
 echo "Include /etc/ssh/sshd_config.d/*.conf" >> /etc/ssh/sshd_config
