6dca8f8897712baa4d8b9c216ee0160ea5cdb120
ilya034 <68101303+ilya034@users.noreply.github.com>
2025-07-04 21:49:58 +0300

    Update basic_config.sh

--- basic_config.sh
+++ basic_config.sh
@@ -5,18 +5,22 @@ set -e
 NEW_USER_NAME=ilya034
 PUBLIC_SSH_KEY=""
 NEW_SSH_PORT=22
+SITE_DOMAIN="contora13.oops.wtf"
+SITE_REPO_URL="https://github.com/ilya034/Contora13Site"
 
 while getopts :u:k:p flag
 do
     case "${flag}" in
-        u) NEW_USER_NAME=${OPTARG};;
+        n) NEW_USER_NAME=${OPTARG};;
         k) PUBLIC_SSH_KEY=${OPTARG};;
-        p) NEW_SSH_PORT=${OPTARG};; 
+        p) NEW_SSH_PORT=${OPTARG};;
+        s) SITE_REPO_URL=${OPTARG};;
+        d) SITE_DOMAIN=${OPTARG};;
     esac
 done
 
 apt update && apt upgrade -y
-apt install htop vim wget curl speedtest-cli gnupg2 ca-certificates lsb-release ubuntu-keyring -y
+apt install git htop vim wget curl speedtest-cli gnupg2 ca-certificates lsb-release ubuntu-keyring -y
 
 curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor \
     | tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null
@@ -43,10 +47,16 @@ cp .vimrc /home/${NEW_USER_NAME}/
 
 mkdir -p /home/${NEW_USER_NAME}/.ssh
 echo "${PUBLIC_SSH_KEY}" > /home/${NEW_USER_NAME}/.ssh/authorized_keys
-chown -R ${NEW_USER_NAME}:${NEW_USER_NAME} /home/${NEW_USER_NAME}/.ssh 
+chown -R ${NEW_USER_NAME}:${NEW_USER_NAME} /home/${NEW_USER_NAME}/.ssh
 chmod 700 /home/${NEW_USER_NAME}/.ssh
 chmod 600 /home/${NEW_USER_NAME}/.ssh/authorized_keys
 
 curl -sLo /etc/ssh/sshd_config.d/updated.conf https://github.com/ilya034/VPSBasicConfigure/raw/master/sshd.conf
 echo "Port ${NEW_SSH_PORT}" >> /etc/ssh/sshd_config.d/updated.conf
 echo "Include /etc/ssh/sshd_config.d/updated.conf" >> /etc/ssh/sshd_config
+
+systemctl restart ssh && systemctl restart sshd
+
+mkdir -p /var/www/${SITE_DOMAIN}/
+git clone ${SITE_REPO_URL} /var/www/${SITE_DOMAIN}
+chown -R www-data:www-data /var/www
